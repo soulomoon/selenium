7a8567fed33a16c67194c8c10d3868d43ed07f86
soulomoon <fwy996602672@gmail.com>
2016-10-13 17:10:16 +0800

    auto exit selenium

--- test.py
+++ test.py
@@ -1,27 +1,11 @@
-import logging
 import multiprocessing
-import signal
+import os
+import subprocess
 from logging import info
-from multiprocessing import Process
-from multiprocessing.pool import Pool
-
-import time
-
-import sys
 
 import psutil
-from multiprocessing import Queue
-
-from multiprocessing import Array
-
-from multiprocessing import Pipe
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from random import randint, random
-import os
-
-import subprocess
 
 
 def info(title):
@@ -32,29 +16,22 @@ def info(title):
 
 
 def run_service():
-    #get process's pid
-    # pid = os.getpid()
-    # if conn:
-    #     conn.send(str(pid))
-    #     conn.close()
 
     info("run_service")
 
     print('service is beginning')
-    selenium_server = subprocess.Popen(['java', '-jar', 'selenium-server-standalone-3.0.0-beta4.jar'])
-    # time.sleep(3)
-    # selenium_server.send_signal(signal.CTRL_C_EVENT)
-    # time.sleep(10)
-    # selenium_server.kill()
+    selenium_server = subprocess.Popen(['java', '-jar', 'selenium-server-standalone-3.0.0-beta4.jar'],
+        shell=True,
+        universal_newlines=True
+    )
     print('service obj returned')
 
     return selenium_server
-    # return selenium_server
 
 
 class Driver:
     # using a driver
-    def __init__(self):
+    def __init__(self, session=None):
         # This is the only code you need to edit in your existing scripts.
         # The command_executor tells the test to run on Sauce, while the desired_capabilities
         # parameter tells us which browsers and OS to spin up.
@@ -65,6 +42,8 @@ class Driver:
 
         self.driver = _driver
         self.session = _driver.session_id
+        if session:
+            self.switch_driver_session(session)
 
     def switch_driver_session(self, new_session):
         _driver = self.driver
@@ -72,6 +51,11 @@ class Driver:
         _driver.session_id = new_session
         self.session = _driver.session_id
 
+    def get_session(self):
+        return self.session
+
+    def close(self):
+        self.driver.quit()
 
     def search_google(self, word):
         # This is your test logic. You can add multiple tests here.
@@ -79,6 +63,7 @@ class Driver:
         _driver.get("http://www.baidu.com")
         if "百度一下" not in _driver.title:
             raise Exception("Unable to load baidu page!")
+
         elem = _driver.find_element_by_name("wd")
         elem.send_keys(word)
         elem.submit()
@@ -93,18 +78,16 @@ class Driver:
 def hello():
     print("hello")
     driver = Driver()
-    driver2 = Driver()
     # driver3 = Driver()
 
     session = driver.driver.session_id
-    session2 = driver2.driver.session_id
 
-    driver2.switch_driver_session(session)
+    driver2 = Driver(session)
 
     driver.search_google("session1")
     driver2.search_google("session2")
 
-    driver.driver.quit()
+    driver.close()
 
     print(driver)
 
@@ -127,59 +110,5 @@ def f(x):
 if __name__ == '__main__':
     # selenium_server = run_service()
     # print(selenium_server)
-    try:
-        info("Parent")
-        pid = os.getpid()
-        #创建程序池
-        # logging.basicConfig(filename='example.log', level=logging.DEBUG)
-        parent_conn, child_conn = Pipe()
-        p = Process(target=run_service, args=(child_conn,))
-        p.start()
-        p2 = Process(target=hello, args=())
-        p2.start()
-
-        time.sleep(3)
-
-        print("child pid"+parent_conn.recv())   # prints "[42, None, 'hello']"
-    finally:
-        p.join()
-
-    try:
-        print(multiprocessing.active_children())
-        info("p")
-
-
-        print("starting the process 1")
-        # q = Queue()
-
-        # n = Array('i', range(10))
-
-        # pro1 = p.apply_async(run_service)
-
-        print(pro1)
-
-        # print("getting q")
-        # print(q.get())
-        #等待服务器创建
-        # pro1.wait(timeout=5)
-        time.sleep(3)
-        print("starting the process 2")
-        pro2 = p.apply_async(hello)
-    finally:
-        print('Waiting for all subprocesses done...')
-        print(d)
-        # p.terminate()
-        kill_proc_tree(pid)
-        print(multiprocessing.active_children())
-
-        p.terminate()
-        p.close()
-        p.join()
-        print('All subprocesses done.')
-        #
-        #
-        # os.system("dir -l")
-    with Pool(4) as p:
-        pass
-
+    pass
 
--- test2.py
+++ test2.py
@@ -1,22 +1,82 @@
+import time
+
 from test import *
 import threading
-if __name__=='__main__':
+
+
+class SelServer:
+
+    def __init__(self):
+        """
+        start a selenium server and wait for it
+        """
+        cmd = self.__get_start_cmd()
+        self.pro = self.__execute_cmd(cmd)
+        self.__wait_start()
+
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+        print(exc_type)
+        # return isinstance(exc_val, exc_type)
+
+    def __get_start_cmd(self):
+        cmd = ['java', '-jar', 'selenium-server-standalone-3.0.0-beta4.jar']
+        return cmd
+
+    @staticmethod
+    def __execute_cmd(cmd) -> subprocess.Popen:
+        """
+        take in a cmd and exicute it in with subprocess.Popen and create a pip line
+
+        :return: Popen instance
+        """
+        pro = subprocess.Popen(cmd,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT,
+                               universal_newlines=True)
+        return pro
+
+    def __wait_start(self):
+        """
+        wait for selenium server set up
+
+        :return: True when selenium server is up False if not
+        """
+        stout = self.pro.stdout
+        server_word = "Selenium Server is up and running"
+        used_word = "java.net.BindException: Address already in use: bind"
+        while True:
+            line = stout.readline()
+            print(line)
+            if server_word in line:
+                print(line)
+                stout.close()
+                return True
+            elif used_word in line:
+                print(line)
+                stout.close()
+                return False
+
+    def close(self):
+        self.pro.terminate()
+
+
+if __name__ == '__main__':
     print("装配线程")
-
     info("this is main")
-    driver = run_service()
 
-    # sel_server = threading.Thread(target=run_service)
 
-    robot = threading.Thread(target=hello)
-    # robot2 = threading.Thread(target=hello)
+    with SelServer() as server:
+        robot = threading.Thread(target=hello)
+        # hello()
+        print("begin")
+        robot.start()
+        time.sleep(3)
+        robot.join()
 
-    # sel_server.start()
-    time.sleep(3)
 
-    robot.start()
-    robot.join()
-
-    driver.kill()
-    # time.sleep(30)
-    # robot2.start()
+    print("done")
